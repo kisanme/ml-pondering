c29873200965b6bc787033c0924180d59324828c
Nasik <nasik2ms@gmail.com>
2020-04-26 15:15:44 +0530

    Computer vision classification of images

---
+++ neural/vision/shoes.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from tensorflow import keras
+
+# load the fashion image data set
+fashion_mnist = keras.datasets.fashion_mnist
+# splits the data into train and test data sets
+# labels are in numbers as well - to avoid bias on languages
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+# build the model
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer=tf.keras.optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# training the model with the dataset
+model.fit(train_images, train_labels, epochs=5)
+
+# evaluate the prediction of our model with the accuracy it tests the test data
+model.evaluate(test_images, test_labels)
+
+# classify the total of test images
+classifications = model.predict(test_images)
+
+print(classifications[0])
+print(test_labels[0])
